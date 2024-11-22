15f15f9ff85cf7ab863ff579f728d451375d8c65
Leanne <lemeyer.no@gmail.com>
2024-11-22 14:16:17 +0100

    Added rollup options to vite config file

--- my-auction-app/vite.config.js
+++ my-auction-app/vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 
@@ -7,4 +8,21 @@ export default defineConfig({
       include: ["src/**/*.{js}, src/js/**/*.{js}"],
     }),
   ],
+  appType: "mpa",
+  base: "",
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        auth: resolve(__dirname, "./auth/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
+        listing: resolve(__dirname, "./listing/index.html"),
+        myListings: resolve(__dirname, "./my_listings/index.html"),
+        editListing: resolve(__dirname, "./listing/edit/index.html"),
+        createListing: resolve(__dirname, "./listing/create/index.html"),
+        myBids: resolve(__dirname, "./my_bids/index.html"),
+      },
+    },
+  },
 });
