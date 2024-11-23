55ab2292c7a71cef1a7b06573ac3db4a93f668d9
Leanne <lemeyer.no@gmail.com>
2024-11-23 21:48:06 +0100

    updated id for all sign in buttons in all html files and added function for displaying and hiding sign in button on side bar nav

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import './styles/main.css';
 import router from './js/router/index.js';
 import { displayLogoutBtn } from './js/ui/global/logout.js';
 import { onclickLogoutBtn } from './js/ui/global/logout.js';
+import { displaySignInBtnNav } from './js/ui/auth/auth.js';
 
 await router(window.location.pathname);
 
 displayLogoutBtn();
 onclickLogoutBtn();
+displaySignInBtnNav();
--- src/js/ui/auth/auth.js
+++ src/js/ui/auth/auth.js
@@ -1,14 +1,16 @@
-const SIGN_IN_BTN = document.getElementById('sign-in-btn');
-const REG_BTN = document.getElementById('reg-btn');
+const SIGN_IN_BTN_NAV = document.getElementById('sign-in-btn-nav');
+const SIGN_IN_BTN_TAB = document.getElementById('sign-in-btn-tab');
+const REG_BTN_TAB = document.getElementById('reg-btn-tab');
 const SIGN_IN_FORM = document.getElementById('sign-in-form');
 const REG_FORM = document.getElementById('register-form');
+const ACCESS_TOKEN = localStorage.getItem('accessToken');
 
 export function onclickRegBtnTab() {
-  REG_BTN.addEventListener('click', displayRegForm);
+  REG_BTN_TAB.addEventListener('click', displayRegForm);
 }
 
 export function onclickSignInBtnTab() {
-  SIGN_IN_BTN.addEventListener('click', displaySignInForm);
+  SIGN_IN_BTN_TAB.addEventListener('click', displaySignInForm);
 }
 
 function displaySignInForm() {
@@ -32,5 +34,13 @@ function displayRegForm() {
   } else {
     REG_FORM.style.display = 'none';
     SIGN_IN_FORM.style.display = 'none';
+  }
+}
+
+export function displaySignInBtnNav() {
+  if (ACCESS_TOKEN) {
+    SIGN_IN_BTN_NAV.style.display = 'none';
+  } else {
+    SIGN_IN_BTN_NAV.style.display = 'block';
   }
 }
