4b8b93ef45e3b54ab4e58219891a530f7f657eb8
Leanne <lemeyer.no@gmail.com>
2024-11-23 10:46:33 +0100

    Adjusted image sizes and renamed them, added html for home page

---
+++ src/js/router/index.js
@@ -0,0 +1,41 @@
+/**
+ * This function controls which JavaScript file is loaded on which page
+ * @async
+ * @function router
+ * @param {string} pathname - The path of the current page.
+ * @returns {Promise<void>}
+ *
+ * @example
+ * // Example of how to call the function
+ * router();
+ *
+ *
+ */
+
+export default async function router(pathname = window.location.pathname) {
+  switch (pathname) {
+    case "/":
+      await import("./views/home.js");
+      break;
+    case "/auth/":
+      await import("./views/auth.js");
+      break;
+    case "/listing/":
+      await import("./views/listing.js");
+      break;
+    case "/create/listing/":
+      await import("./views/createListing.js");
+      break;
+    case "/edit/listing/":
+      await import("./views/editListing.js");
+      break;
+    case "/my/bids/":
+      await import("./views/myBids.js");
+      break;
+    case "/profile/":
+      await import("./views/profile.js");
+      break;
+    default:
+      await import("./views/notFound.js");
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue,html}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue,html}", "index.html"],
   theme: {
     extend: {},
   },
