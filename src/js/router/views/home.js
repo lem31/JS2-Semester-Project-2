1f2ba6ad0dc077028b115fcf771c416c1b47990d
Leanne <lemeyer.no@gmail.com>
2024-12-03 21:10:21 +0100

    removed unused function imported into home.js

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,6 +1,5 @@
 import { getAllArtAuctionListings } from '../../api/listing/read.js';
 import { onClickSearchButton } from '../../components/search.js';
-import { placeBid } from '../../ui/bids/place.js';
 
 getAllArtAuctionListings();
 onClickSearchButton();
