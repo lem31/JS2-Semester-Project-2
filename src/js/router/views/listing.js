bc3b83bd486835773a110bde66845e09bcef381c
Leanne <lemeyer.no@gmail.com>
2024-12-10 10:50:11 +0100

    Added styles to nav sidebar on listing page and added functions for the sidebar

--- src/js/router/views/listing.js
+++ src/js/router/views/listing.js
@@ -1,3 +1,7 @@
 import { displayIndividualListing } from '../../api/listing/read';
+import { displayNav } from '../../components/sidebar.js';
+import { onHoverNavLink } from '../../components/sidebar.js';
 
 displayIndividualListing();
+displayNav();
+onHoverNavLink();
