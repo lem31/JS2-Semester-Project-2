087530981b6982b90be9519e1ad4bd49d88a80c6
Leanne <lemeyer.no@gmail.com>
2025-06-09 10:09:26 +0200

    fixed edit listing form and sign out button

--- src/js/ui/listing/edit.js
+++ src/js/ui/listing/edit.js
@@ -25,30 +25,68 @@ export function displayListingIdInUrlOnEditPage(event) {
 export function onClickAddMoreImages() {
   const ADD_MORE_IMAGES_BTN = document.getElementById('Add-image-button-edit');
   if (ADD_MORE_IMAGES_BTN) {
-    ADD_MORE_IMAGES_BTN.addEventListener('click', (event) => {
-      event.preventDefault();
-      const EDIT_FORM = document.getElementById('edit-form');
+    ADD_MORE_IMAGES_BTN.addEventListener('click', () => {
+      const URL_CONTAINER = document.getElementById('url-container');
+const URL_DIV = document.createElement('div');
+const ALT_DIV = document.createElement('div');
+      URL_DIV.classList.add('flex', 'flex-col', 'align-items-start', 'mb-2');
+      ALT_DIV.classList.add('flex', 'flex-col', 'align-items-start', 'mb-2');
+
+      const IMAGE_INPUT_WRAPPER = document.createElement('div');
+      IMAGE_INPUT_WRAPPER.classList.add('image-input-wrapper', 'flex', 'flex-col', 'items-start', 'mb-2');
+
+
+      const URL_LABEL = document.createElement('label');
+      URL_LABEL.setAttribute('for', 'urls');
+      URL_LABEL.textContent = 'Image URL';
+      URL_LABEL.classList.add('gold-labels', 'mr-2');
+
       const URL_INPUT = document.createElement('input');
       URL_INPUT.setAttribute('type', 'url');
       URL_INPUT.setAttribute('name', 'urls');
       URL_INPUT.setAttribute('placeholder', 'Image URL');
-      URL_INPUT.setAttribute('class', 'input');
-      URL_INPUT.classList.add('input-styles');
-      EDIT_FORM.appendChild(URL_INPUT);
+      URL_INPUT.setAttribute('class', 'input input-styles mt-2 mb-2 mr-2');
+      URL_INPUT.setAttribute('id', 'urlInput');
+
+
+      const ALT_LABEL = document.createElement('label');
+      ALT_LABEL.setAttribute('for', 'alts');
+      ALT_LABEL.textContent = 'Image Alt';
+      ALT_LABEL.classList.add('gold-labels', 'mr-2');
+
       const ALT_INPUT = document.createElement('input');
       ALT_INPUT.setAttribute('type', 'text');
       ALT_INPUT.setAttribute('name', 'alts');
       ALT_INPUT.setAttribute('placeholder', 'Image Alt');
-      ALT_INPUT.setAttribute('class', 'input');
-      const ADD_MORE_IMAGES_BTN = document.getElementById(
-        'Add-image-button-edit'
-      );
-      EDIT_FORM.insertBefore(URL_INPUT, ADD_MORE_IMAGES_BTN);
-      EDIT_FORM.insertBefore(ALT_INPUT, URL_INPUT);
-      ALT_INPUT.classList.add('input-styles');
+      ALT_INPUT.setAttribute('class', 'input input-styles mt-2 mb-2 mr-2');
+      ALT_INPUT.setAttribute('id', 'altInput');
+
+const REMOVE_BTN_DIV = document.createElement('div');
+      REMOVE_BTN_DIV.classList.add('flex', 'justify-start', 'w-full');
+      const REMOVE_BTN = document.createElement('button');
+      REMOVE_BTN.type = 'button';
+      REMOVE_BTN.textContent = 'Remove';
+      REMOVE_BTN.classList.add('button-styles', 'ml-2');
+      REMOVE_BTN.addEventListener('click', () => {
+      IMAGE_INPUT_WRAPPER.remove();
+      });
+URL_DIV.appendChild(URL_LABEL);
+      URL_DIV.appendChild(URL_INPUT);
+      ALT_DIV.appendChild(ALT_LABEL);
+      ALT_DIV.appendChild(ALT_INPUT);
+REMOVE_BTN_DIV.appendChild(REMOVE_BTN);
+      IMAGE_INPUT_WRAPPER.appendChild(URL_DIV);
+
+      IMAGE_INPUT_WRAPPER.appendChild(ALT_DIV);
+
+      IMAGE_INPUT_WRAPPER.appendChild(REMOVE_BTN_DIV);
+
+
+      URL_CONTAINER.appendChild(IMAGE_INPUT_WRAPPER);
     });
   }
 }
+
 export function onClickSaveListingBtn() {
   const EDIT_FORM = document.getElementById('edit-form');
 
